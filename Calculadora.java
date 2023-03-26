198cb25d18e4b3cc553ec41e2c08aaee62c4ca1c
Usuroberto <robertohevia@ejemplo.es>
2023-03-26 21:30:43 +0200

    Creo mi primera versión del proyecto AE4202223

---
+++ Calculadora.java
@@ -0,0 +1,33 @@
+package Actividad4;
+
+public class Calculadora 
+{
+
+	public static void main(String[] args) 
+	{
+	       int numero1;
+	       int numero2;
+	       int suma, resta, multiplicacion, division;
+	       
+
+	        System.out.print("Introduzca un número: ");
+	        numero1 = Teclado.entero();
+
+	        System.out.print("Introduzca otro número: ");
+	        numero2 = Teclado.entero();
+
+	        suma = numero1 + numero2;
+	        System.out.println("La suma de " + numero1 + " + " + numero2 + " es: " + suma);
+	        
+	        resta = numero1 - numero2;
+	        System.out.println("La resta de " + numero1 + " - " + numero2 + " es: " + resta);
+	        
+	        multiplicacion = numero1 * numero2;
+	        System.out.println("La multiplicación de " + numero1 + " * " + numero2 + " es: " + multiplicacion);
+	        
+	        division = numero1 / numero2;
+	        System.out.println("La división de " + numero1 + " / " + numero2 + " es: " + division);
+
+	}
+
+}
